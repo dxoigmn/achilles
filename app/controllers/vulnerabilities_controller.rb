1023b04ad370c551cd3afecbd2c6601ec1086d75
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-03-04 08:32:15 -0500

    Added some more style.

--- app/controllers/vulnerabilities_controller.rb
+++ app/controllers/vulnerabilities_controller.rb
@@ -13,7 +13,7 @@ class VulnerabilitiesController < ApplicationController
   # GET /vulnerabilities/1
   # GET /vulnerabilities/1.xml
   def show
-    @vulnerability = Vulnerability.find(params[:id], :include => :plugin)
+    @vulnerability = Vulnerability.find(params[:id], :include => [:plugin, :host])
 
     respond_to do |format|
       format.html # show.html.erb
