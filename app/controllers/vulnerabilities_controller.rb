04968d7b0a4ef67c7c14b79866f11362d425d280
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-03-07 03:14:25 -0500

    Get rid of vulnerabilities index.

--- app/controllers/vulnerabilities_controller.rb
+++ app/controllers/vulnerabilities_controller.rb
@@ -1,15 +1,4 @@
 class VulnerabilitiesController < ApplicationController
-  # GET /vulnerabilities
-  # GET /vulnerabilities.xml
-  def index
-    @vulnerabilities = Vulnerability.find(:all, :include => :plugin)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @vulnerabilities }
-    end
-  end
-
   # GET /vulnerabilities/1
   # GET /vulnerabilities/1.xml
   def show
