ca97c6e58a92f02ebc1d8d439acd7ae8a3ed3373
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-04-03 21:31:00 -0400

    Fix fixtures.

---
+++ db/fixtures/setup.rb
@@ -0,0 +1,23 @@
+include FixtureHelpers
+
+status :name => 'Open (Needs Remediation)', :default => true
+status :name => 'Closed (Remediated)'
+status :name => 'Logged (No Remediation Necessary)'
+
+localize :name => 'world',               :cidr => '0.0.0.0/0',         :as => 'World'
+
+classification :name => 'Compromised'
+classification :name => 'Full Control - Remote'
+classification :name => 'Full Control - Local'
+classification :name => 'System User Access'
+classification :name => 'DoS - System'
+classification :name => 'DoS - Service'
+classification :name => 'Degradation - System'
+classification :name => 'Degradation - Service'
+classification :name => 'Data Disclosure - Full'
+classification :name => 'Data Disclosure - Partial'
+classification :name => 'Data Corruption'
+classification :name => 'System Info Disclosure'
+classification :name => 'Service Control'
+
+user :name => '', :locations => ['World']
--- db/fixtures/statuses.rb
+++
@@ -1,5 +0,0 @@
-include FixtureHelpers
-
-status :name => 'Open (Needs Remediation)', :default => true
-status :name => 'Closed (Remediated)'
-status :name => 'Logged (No Remediation Necessary)'
