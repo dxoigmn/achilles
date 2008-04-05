45e74573e49bce72883f7964ce1a14f21364592e
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-04-05 16:37:26 -0400

    Fix haml deprecation notice.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def nav_link(name)
-    open(:li, {:class => (controller.kind_of?("#{name.to_s.camelize}Controller".constantize) ? 'active' : 'inactive' )}) do
+    haml_tag(:li, {:class => (controller.kind_of?("#{name.to_s.camelize}Controller".constantize) ? 'active' : 'inactive' )}) do
       puts link_to(name.to_s.titleize, self.send("#{name.to_s.tableize}_path"))
     end
   end
