288241f4e171944d6a10d8605899e88798dda597
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-05-08 05:20:11 -0400

    DRY up form elements.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def form_field(label, field)
+    haml_tag(:dt) do
+      haml_tag(:div, label.to_s)
+    end
+    
+    haml_tag(:dd, find_and_preserve(field))
+  end
+  
   def nav_link(name)
     haml_tag(:li, {:class => (controller.kind_of?("#{name.to_s.camelize}Controller".constantize) ? 'active' : 'inactive' )}) do
       puts link_to(name.to_s.titleize, self.send("#{name.to_s.tableize}_path"))
