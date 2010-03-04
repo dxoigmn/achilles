898038d04c66ef29e4c0091fe09db2ea7dc79bb8
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2010-03-04 16:48:44 -0500

    Use specific dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '>= 2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,10 +26,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'haml', :version => '>= 2.0.6'
-  config.gem 'chronic', :version => '>= 0.2.3'
-  config.gem 'netaddr', :version => '>= 1.5.0'
-  config.gem 'libxml-ruby', :version => '>= 0.9.7', :lib => 'xml/libxml'
+  config.gem 'haml', :version => '2.0.6'
+  config.gem 'chronic', :version => '0.2.3'
+  config.gem 'netaddr', :version => '1.5.0'
+  config.gem 'libxml-ruby', :version => '0.9.7', :lib => 'xml/libxml'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
